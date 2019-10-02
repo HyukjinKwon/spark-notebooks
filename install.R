dbf0222492331f88f934701db47397b58616dd3c
HyukjinKwon <gurwls223@apache.org>
2019-10-02 13:35:45 +0900

    test

---
+++ install.R
@@ -0,0 +1,4 @@
+install.packages(c('knitr', 'rmarkdown', 'e1071', 'survival'), repos='https://cloud.r-project.org/')
+install.packages(c('crayon', 'praise', 'R6', 'arrow'), repos='https://cloud.r-project.org/')
+install.packages('https://cloud.r-project.org/src/contrib/Archive/testthat/testthat_1.0.2.tar.gz', repos=NULL, type='source')
+
