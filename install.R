50107c51a39a45d157205fcd1f88957ef24b6ade
HyukjinKwon <gurwls223@apache.org>
2019-10-02 14:41:13 +0900

    testthat

--- install.R
+++ install.R
@@ -1,4 +1,5 @@
 install.packages(c('knitr', 'rmarkdown', 'e1071', 'survival', 'roxygen2', 'arrow'), repos='https://cloud.r-project.org/')
 library(arrow)
 arrow::install_arrow()
-
+install.packages(c('crayon', 'praise', 'R6'), repos='https://cloud.r-project.org/')
+install.packages('https://cloud.r-project.org/src/contrib/Archive/testthat/testthat_1.0.2.tar.gz', repos=NULL, type='source')
