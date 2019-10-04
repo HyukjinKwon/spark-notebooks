2860c153b6d0f118dd59e88044031617cb1aba98
HyukjinKwon <gurwls223@apache.org>
2019-10-04 16:47:23 +0900

    demo

--- install.R
+++ install.R
@@ -1,3 +1,4 @@
 install.packages(c('knitr', 'rmarkdown', 'e1071', 'survival', 'roxygen2'), repos='https://cloud.r-project.org/')
 install.packages(c('crayon', 'praise', 'R6'), repos='https://cloud.r-project.org/')
 install.packages('https://cloud.r-project.org/src/contrib/Archive/testthat/testthat_1.0.2.tar.gz', repos=NULL, type='source')
+install.packages(c('microbenchmark', 'ggplot2'), repos='https://cloud.r-project.org/')
